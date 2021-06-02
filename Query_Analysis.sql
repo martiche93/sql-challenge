70e7e5130899145c64b9a89abb07303acdd5f67b
Cheyenne M <76500260+martiche93@users.noreply.github.com>
2021-06-01 21:14:59 -0700

    Update Query_Analysis.sql
    updating code

--- Query_Analysis.sql
+++ Query_Analysis.sql
@@ -1,6 +1,10 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender,salaries.salaries
+SELECT employees.emp_no, 
+employees.last_name, 
+employees.first_name, 
+employees.gender,
+salaries.salaries
 FROM employees
 LEFT JOIN salaries
 ON employees.emp_no = salaries.emp_no
